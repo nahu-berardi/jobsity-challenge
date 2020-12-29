fcc18a60540bfa566996720037a8dc1165c0c7f1
Marcos Berardi <marcos.berardi@southworks.com>
2020-12-29 00:20:55 -0300

    Number of reminders icon

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Grid, Paper, Tooltip } from "@material-ui/core";
+import { Badge, Grid, Paper, Tooltip } from "@material-ui/core";
 import moment from 'moment';
 import * as utils from "./utils";
 import './Body.scss';
+import { useStore } from 'react-redux';
+import { Event } from '@material-ui/icons';
 
 export function Body(props) {
     const { display, detailed, selectedDate } = props;
+
+    const reminders = useStore().getState().reminders.reminder;
 
     return (
         <Paper style={{ display: 'flex', height: 'calc(100% - 56px)', width: '100%' }}>
@@ -16,7 +20,7 @@ export function Body(props) {
                             <Tooltip title={moment(detailed[columnIndex][dayIndex]).format('LL')}>
                                 <Grid
                                     item
-                                    style={{ height: '100%', width: '100%', padding: '16px', border: '1px solid gray', cursor: 'pointer' }}
+                                    style={{ position: 'relative', height: '100%', width: '100%', padding: '16px', border: '1px solid gray', cursor: 'pointer' }}
                                     className={utils.calendarGridClassName(columnIndex)}
                                     onClick={_ => selectedDate(moment(detailed[columnIndex][dayIndex]).toISOString(true))}
                                 >
@@ -27,12 +31,26 @@ export function Body(props) {
                                     >
                                         {origColumn[dayIndex]}
                                     </p>
+                                    {reminders[detailed[columnIndex][dayIndex].substr(0, 10)] &&
+                                        reminders[detailed[columnIndex][dayIndex].substr(0, 10)].length ?
+                                        (<div style={{ position: 'absolute', bottom: 0, right: 12 }}>
+                                            <Badge
+                                                color="primary"
+                                                badgeContent={reminders[detailed[columnIndex][dayIndex].substr(0, 10)].length}
+                                                max={9}
+                                            >
+                                                <Event />
+                                            </Badge>
+                                        </div>) :
+                                        (<></>)
+                                    }
                                 </Grid>
                             </Tooltip>
                         );
                     })}
                 </Grid>
-            ))}
-        </Paper>
+            ))
+            }
+        </Paper >
     );
 }
